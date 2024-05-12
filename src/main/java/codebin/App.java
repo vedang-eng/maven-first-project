b3ae436fc3148581689bcb0cd5f005077b74435e
Vedang Surnis <vedang.surnis@gmail.com>
2024-05-12 23:47:43 +0530

    First maven project

---
+++ src/main/java/codebin/App.java
@@ -0,0 +1,7 @@
+package codebin;
+
+public class App {
+	public String sample() {
+		return "same code";
+	}
+}
---
+++ src/test/java/codebin/AppunitTest.java
@@ -0,0 +1,14 @@
+package codebin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppunitTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+
+}
